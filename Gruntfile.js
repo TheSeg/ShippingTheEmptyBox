6f4530a3e48103a6a76bc6f170a7367ebd750177
John *Seg* Seggerson <seg@segonmedia.com>
2014-09-07 17:42:44 -0700

    * Added grunt contribs. * Added baseline custom style. * Added baseline custom JavaScript. * Updated GitIgnore * Updated github-pages gem.

---
+++ Gruntfile.js
@@ -0,0 +1,99 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed Creative Commons (CA-BY-SA) */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['_assets/scripts/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      dist_preconcat: {
+        src: 'assets/scripts/**/*.js'
+      },
+      dist_postconcat: {
+        src: '<%= uglify.dist.dest %>'
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          unixNewlines:true,
+          style:'compact'
+        },
+        files: {
+          "dist/<%= pkg.name %>.css":"_assets/style/app.scss"
+        }
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test']
+      },
+      js: {
+        files: '<%= jshint.dist.src %>',
+        tasks: [ 'jshint:dist_preconcat', 'uglify:dist', 'jshint:dist_postconcat']
+      },
+      style: {
+        files: '_assets/style/**/*.scss',
+        tasks: ['sass:dist']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'sass']);
+
+};
