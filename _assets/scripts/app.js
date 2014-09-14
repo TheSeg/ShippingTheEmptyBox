1ff96b62ffc5ee7d496c4ec6d02f3d3b61e1a8cc
John *Seg* Seggerson <seg@segonmedia.com>
2014-09-14 16:20:06 -0700

    Removed console.log()

--- _assets/scripts/app.js
+++ _assets/scripts/app.js
@@ -3,7 +3,6 @@
   
   // Grunticon
   var baseurl = $("meta[property='baseurl']").attr("content");
-  console.log(baseurl);
   var grunticonRoot = baseurl+'dist/svg/';
   grunticon([ grunticonRoot+'icons.data.svg.css', grunticonRoot+'icons.data.png.css', grunticonRoot+'icons.fallback.css' ]);
   
