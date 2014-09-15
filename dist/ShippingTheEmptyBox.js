9e99084957445396d936762cc3dd2c552f93caa7
John *Seg* Seggerson <seg@segonmedia.com>
2014-09-15 00:46:30 -0700

    * Realized I need to add the dist folder in order to actually include assets.

---
+++ dist/ShippingTheEmptyBox.js
@@ -0,0 +1,11 @@
+/*! ShippingTheEmptyBox - v0.1.0
+* https://github.com/TheSeg/ShippingTheEmptyBox
+* Copyright (c) 2014 ; Licensed Creative Commons (CA-BY-SA) */
+(function($){
+  
+  // Grunticon
+  var baseurl = $("meta[property='baseurl']").attr("content");
+  var grunticonRoot = baseurl+'dist/svg/';
+  grunticon([ grunticonRoot+'icons.data.svg.css', grunticonRoot+'icons.data.png.css', grunticonRoot+'icons.fallback.css' ]);
+  
+})(jQuery);
